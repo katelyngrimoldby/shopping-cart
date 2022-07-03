617bb42cc952893097c51074d62a7f7c8f1f7f2f
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-03 15:57:07 -0600

    Initial commit

---
+++ src/App.test.js
@@ -0,0 +1,4 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
+import TestComponent from 'path-to-test-component';
