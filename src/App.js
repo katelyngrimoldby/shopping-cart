796f36bf50e284942ec10d1d6ab4277eef698301
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-04 14:04:54 -0600

    Add basic setup and state logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <></>;
-}
+import { useState } from 'react';
+import Card from './components/Card';
 
+function App() {
+  const [cart, setCart] = useState([]);
+  const products = ['Item 1', 'Item 2', 'Item 3'];
+
+  const onSubmit = (data) => {
+    setCart([...cart, data]);
+  };
+
+  return (
+    <>
+      {products.map((e, i) => {
+        return <Card key={i} product={e} submit={onSubmit} />;
+      })}
+    </>
+  );
+}
 export default App;
