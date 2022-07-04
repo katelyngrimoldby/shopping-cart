1e84f98264f7a3ce730d561aef3da4c0fa9ae4b8
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-04 14:24:32 -0600

    Complete add to cart logic

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ function App() {
   const products = ['Item 1', 'Item 2', 'Item 3'];
 
   const onSubmit = (data) => {
-    setCart([...cart, data]);
+    const currentCart = cart;
+    if (currentCart.find((e) => e.product === data.product)) {
+      const product = currentCart.find((e) => e.product === data.product);
+      product.quantity += data.quantity;
+      const newCart = [product].concat(
+        ...currentCart.filter((e) => e.product !== product.product)
+      );
+      setCart(newCart);
+    } else {
+      setCart([data, ...cart]);
+    }
   };
 
   return (
