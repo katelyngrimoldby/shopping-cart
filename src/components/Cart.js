715a5dd80ce3196fafc6ada4ca5b1d6c7a4be39c
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-04 20:07:01 -0600

    Add delete btn to items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,32 @@
-function CartItem({ product, quantity }) {
+function CartItem({ product, quantity, remove }) {
   return (
     <div>
       <h2>{product}</h2>
       <span>{quantity}</span>
+      <button
+        type="button"
+        id="remove"
+        name="remove"
+        onClick={() => remove({ product, quantity })}
+      >
+        Remove
+      </button>
     </div>
   );
 }
 
-function Cart({ content }) {
+function Cart({ content, callback }) {
   return (
     <div>
       {content.map((e, i) => {
-        return <CartItem key={i} product={e.product} quantity={e.quantity} />;
+        return (
+          <CartItem
+            key={i}
+            product={e.product}
+            quantity={e.quantity}
+            remove={() => callback(e)}
+          />
+        );
       })}
     </div>
   );
